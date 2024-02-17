a5f84ffd6964e82a26e5095245d6942d55558b59
Maksim <maksim.bobrikov2015@yandex.ru>
2024-02-18 01:07:51 +0300

    db appeared

---
+++ Site_store/User_set/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path("reg/", views.registration, name="new_user"),
+    # path("login/", views.login, name="login"),
+    # path("logout/", views.logout, name="logout"),
+    # path("notes/", views.notes, name="notes"),
+    # path("add_note/", views.add_note, name="new_note"),
+]
