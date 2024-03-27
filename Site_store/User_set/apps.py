cba8a2dfd85d558a35a4708b6bbc8af78a166c42
Maksim <maksim.bobrikov2015@yandex.ru>
2024-03-27 22:00:20 +0300

    Profiles fixed

--- Site_store/User_set/apps.py
+++ Site_store/User_set/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UserSetConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'User_set'
+
+    def ready(self):
+        import User_set.signals
