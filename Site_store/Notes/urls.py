4a9f68d5a911f648f2cfbda79389d7e9f45fd52d
Maksim <maksim.bobrikov2015@yandex.ru>
2024-03-30 15:42:00 +0300

    Notes page appeared

--- Site_store/Notes/urls.py
+++ Site_store/Notes/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
 	path('', views.home, name='store-home'),
 	path('about/', views.about, name='about-site'),
+	path('notes/', views.notes, name='notes'),
 ]
--- Site_store/Notes/views.py
+++ Site_store/Notes/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from .models import Notes
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
-    context = {
-        'notes': Notes.objects.all()
-    }
-    return render(request, 'home.html', context)
-
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html', {'title': 'О магазине PROASS'})
+
+@login_required
+def notes(request):
+    context = {
+        'notes': Notes.objects.filter(author=request.user)
+    }
+    return render(request, 'my_notes.html', context)
